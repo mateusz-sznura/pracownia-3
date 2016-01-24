a180947a6aa6387429e5f40542de6ea7d2a592a4
Mateusz Sznura <msznura@gmail.com>
2016-01-24 14:36:40 +0100

    Minor changes

--- my_malloc.c
+++ my_malloc.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 
+#include "my_malloc.h"
+
 // int main(int argc, char** argv)
 // {
 //  return 0;
--- my_malloc.h
+++ my_malloc.h
@@ -1,4 +1,9 @@
+#ifndef _MY_MALLOC_H_
+#define _MY_MALLOC_H_
+
 void *malloc(size_t size);
 void *calloc(size_t count, size_t size);
 void *realloc(void *ptr, size_t size);
 void free(void *ptr);
+
+#endif
